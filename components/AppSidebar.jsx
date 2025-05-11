68b3d1d893540f44c9a9f24d30fd3425a6c1c63c
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-05-11 13:49:22 +0530

    sidebar done

--- components/AppSidebar.jsx
+++ components/AppSidebar.jsx
@@ -6,34 +6,45 @@ import {
     SidebarGroupContent,
     SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
   } from "@/components/ui/sidebar"
 import Image from "next/image"
 import { Button } from "./ui/button"
-import { LayoutDashboardIcon, VideoIcon } from "lucide-react"
+import { LayoutDashboardIcon, Settings, VideoIcon, Videotape, WalletCards } from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const sidebarItems = [
   {
     title: "Dashboard",
     icon: LayoutDashboardIcon,
-    path: "/dashboard",
+    path: "/workspace",
   },
   {
     title: " Create Ad ",
     icon: VideoIcon,
     path: "/workspace/create-ad",
   },{
-    title: " Create Ad ",
-    icon: VideoIcon,
-    path: "/workspace/create-ad",
-  }, 
-  {
+    title: " My Ads ",
+    icon: Videotape,
+    path: "/workspace/my-ads",
+  },  {
     title: "Settings",
-    icon: "/icons/settings.svg",
+    icon: Settings,
+    path: "/workspace/settings",
+  },
+  {
+    title: "Billing",
+    icon: WalletCards,
+    path: "/workspace/billing",
   },
   
 ]
   
   export function AppSidebar() {
+    const path=usePathname(); 
     return (
       <Sidebar>
         <SidebarHeader >
@@ -46,10 +57,25 @@ const sidebarItems = [
           </SidebarGroup>
           <SidebarGroup >
             <SidebarGroupLabel>
-
+              <h2 className="text-sm font-semibold text-muted-foreground">Application</h2>
             </SidebarGroupLabel>
             <SidebarGroupContent>
+              <SidebarMenu>
+                {sidebarItems.map((item,index) => (
+                  <SidebarMenuItem
 
+                    key={index}
+                    
+                  >
+                    <SidebarMenuButton asChild className={"p-5"}>
+                      <Link href={item.path} className={`flex items-center text-[20px] text-primary-muted hover:text-primary ${path==item.path&&'text-primary bg-voilet-100'}`}>
+                        <item.icon className="mr-2 h-10 w-10" />
+                        <span>{item.title}</span>
+                        </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
             </SidebarGroupContent>
             </SidebarGroup>
         </SidebarContent>
